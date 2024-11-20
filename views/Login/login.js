f0300eddc3193974e7d30949db180f380961536b
ismaelce122 <ismaelce@hotmail.com>
2024-11-20 16:37:48 -0300

    Atualizacões página login e shoping card

--- views/Login/login.js
+++ views/Login/login.js
@@ -14,3 +14,37 @@ const mudarIcone = () => {
         iconeOlho.title = 'mostrar senha'
     }
 }
+
+const Inscrever = () => {
+    const divInscrever = document.getElementById('div_inscrever')
+    const divEntrar = document.getElementById('div_entrar')
+    const btnInscrever = document.getElementById('btn_inscrever')
+    const btnEntrar = document.getElementById('btn_entrar')
+
+    divInscrever.classList.remove('tab3')
+    divEntrar.classList.remove('tab2')
+    divInscrever.classList.add('tab2')
+    divEntrar.classList.add('tab3')
+
+    btnEntrar.classList.remove('sign-in')
+    btnInscrever.classList.remove('sign-in2')
+    btnEntrar.classList.add('sign-in2')
+    btnInscrever.classList.add('sign-in')
+}
+
+const Entrar = () => {
+    const divInscrever = document.getElementById('div_inscrever')
+    const divEntrar = document.getElementById('div_entrar')
+    const btnInscrever = document.getElementById('btn_inscrever')
+    const btnEntrar = document.getElementById('btn_entrar')
+
+    divInscrever.classList.remove('tab2')
+    divEntrar.classList.remove('tab3')
+    divInscrever.classList.add('tab3')
+    divEntrar.classList.add('tab2')
+
+    btnEntrar.classList.remove('sign-in2')
+    btnInscrever.classList.remove('sign-in')
+    btnEntrar.classList.add('sign-in')
+    btnInscrever.classList.add('sign-in2')
+}
